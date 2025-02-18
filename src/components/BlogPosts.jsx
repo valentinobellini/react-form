0136acc759b5180f67c909c6d635ba34c6af57a5
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-18 15:24:22 +0100

    create blog list + from input to add posts

--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -1,48 +1,59 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 const blogPosts = [
-    `"1. Minimalismo Digitale: Come Ridurre il Rumore Online"
-     In un mondo sempre più connesso, imparare a ridurre le distrazioni digitali è essenziale. Scopri strategie per semplificare la tua vita online e concentrarti su ciò che conta davvero.`,
+    `Minimalismo Digitale: Come Ridurre il Rumore Online`,
 
-    `"2. Il Potere del Colore nel Web Design"
-     I colori influenzano le emozioni e il comportamento degli utenti. Scopri come scegliere la palette giusta per il tuo sito e creare un’esperienza visiva efficace.`,
+    `Il Potere del Colore nel Web Design`,
 
-    `"3. Fotografia Documentaria: Raccontare Storie con le Immagini"
-     La fotografia è un potente strumento narrativo. Ecco alcuni consigli per catturare immagini che parlano e comunicano emozioni autentiche.`,
+    `Fotografia Documentaria: Raccontare Storie con le Immagini`,
 
-    `"4. JavaScript per Principianti: Funzioni Essenziali da Conoscere"
-     Sei agli inizi con JavaScript? Ecco alcune funzioni fondamentali che ti aiuteranno a migliorare il tuo codice e scrivere programmi più efficienti.`,
+    `JavaScript per Principianti: Funzioni Essenziali da Conoscere`,
 
-    `"5. Come Creare un Portfolio Online Efficace"
-     Un portfolio ben strutturato è essenziale per attrarre clienti. Scopri gli elementi chiave per mostrare il tuo lavoro al meglio e lasciare un’impressione duratura.`
+    `Come Creare un Portfolio Online Efficace`
 ];
 
 
 
 export default function BlogPost() {
 
+    // stato della lista posts
+    const [posts, setPosts] = useState(blogPosts)
+    // stato dell'input inserimento post
+    const [newPost, setNewPost] = useState('');
+
+    // funzione per l'aggiunta di un nuovo post da input
+    const addPost = e => {
+        e.preventDefault();
+        const updatedPost = [...posts, newPost];
+        setPosts(updatedPost);
+        // azzeriamo il valore di newPost in input
+        setNewPost('');
+    }
 
 
     return (
-        <ul>
-            {blogPosts.map((post, i) => (
-                <li className="post" key={i}>
-                    {/* <h3>{post.title}</h3> */}
-                    <p>{post}</p>
-                    <div className="post-lower-wrapper">
-                        <div className="left">
-                            {/* <p className="author">{post.author}</p>
-                            <p className="date">{post.date}</p> */}
-                        </div>
-                        {/* <ul className="tags">
-                            {post.tags.map((tag) => (
-                                <li key={tag}>{tag + " /"}</li>
-                            ))}
-                        </ul> */}
-                    </div>
-                </li>
-            ))}
-        </ul>
 
+        <>
+            {/* form per aggiunta post */}
+            <form onSubmit={addPost}>
+                <input type="text" value={newPost}
+                    onChange={event => { setNewPost(event.target.value) }}
+                />
+                <button>Invia</button>
+            </form >
+
+
+            {/* lista post */}
+            <ul>
+                {posts.map((post, i) => (
+                    <li className="post" key={i}>
+                        {/* <h3>{post.title}</h3> */}
+                        <p>{post}</p>
+                        <div className="post-lower-wrapper">
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
